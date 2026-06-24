08e4ad6395cc3673e7196ebed4ef07c83ef3311e
Pedro Kopper <pedro.kopper@microchip.com>
2026-06-24 13:19:01 +0100

    meta-mchp-polarfire: apply CPU opp dtbo by default for Video kit
    Apply the CPU operating point overlay by default for the Video kit.
    This is the same as Icicle. Cleaned up comments and bogus fdt addr call.
    
    Signed-off-by: Pedro Kopper <pedro.kopper@microchip.com>

--- meta-mchp-polarfire-soc/meta-mchp-polarfire-soc-bsp/recipes-bsp/u-boot/files/mpfs-video-kit/boot.cmd
+++ meta-mchp-polarfire-soc/meta-mchp-polarfire-soc-bsp/recipes-bsp/u-boot/files/mpfs-video-kit/boot.cmd
@@ -1,5 +1,6 @@
+setenv fitconf conf-mpfs-video-kit.dtb
 load mmc 0:${distro_bootpart} ${scriptaddr} fitImage
-bootm start ${scriptaddr}#conf-mpfs-video-kit.dtb;
+bootm start ${scriptaddr}#${fitconf}#conf-mpfs_video_cpu_opp.dtbo
 bootm loados ${scriptaddr};
 # Try to load a ramdisk if available inside fitImage
 bootm ramdisk;
